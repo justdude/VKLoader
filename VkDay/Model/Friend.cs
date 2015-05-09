65dcbc19997a7bd5fae93965d3e9a7a4979048df
dude_just_dude <dude.just.dude@gmai.com>
2015-05-09 09:12:36 +0300

    refactor

---
+++ VkDay/Model/Friend.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VkDay.Model
+{
+	public class Friend
+	{
+		public int id { get; set; }
+		public string first_name { get; set; }
+		public string last_name { get; set; }
+		public string nickname { get; set; }
+		public string sex { get; set; }
+		public string birthdate { get; set; }
+		public string city { get; set; }
+		public string country { get; set; }
+		public string timezone { get; set; }
+		public string photo { get; set; }
+		public string photo_medium { get; set; }
+		public string photo_big { get; set; }
+	}
+}
---
+++ VkDay/Model/Wall.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VkDay.Model
+{
+	public class Wall
+	{
+		public int id { get; set; }
+		public int from_id { get; set; }
+		public int to_id { get; set; }
+		public int date { get; set; }
+		public string text { get; set; }
+		public Object comments { get; set; }
+		public Object likes { get; set; }
+		public Object reposts { get; set; }
+	}
+}
