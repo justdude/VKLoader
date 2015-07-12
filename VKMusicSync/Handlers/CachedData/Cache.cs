5390ebad98acba4ebd74acbe03eb40293502a391
dude_just_dude <dude.just.dude@gmai.com>
2015-07-12 19:50:47 +0300

    no message

--- VKMusicSync/Handlers/CachedData/Cache.cs
+++ VKMusicSync/Handlers/CachedData/Cache.cs
@@ -2,27 +2,50 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Xml.Linq;
 
 namespace VKMusicSync.Handlers.CachedData
 {
 	public static class Cache
 	{
-		public static Dictionary<object, object> Items = new Dictionary<object, object>();
+		public static readonly Dictionary<object, object> Items = new Dictionary<object, object>();
+
+		//public static readonly Dictionary<object, string> ItemsPath = new Dictionary<object, object>();
+
+		private static readonly object modSyncObject = new object();
+
 
 		public static void AddIfNotExist(object key, object value)
 		{
-			if (Items.ContainsKey(key))
-				return;
+			lock (modSyncObject)
+			{
+				if (Items.ContainsKey(key))
+					return;
 
-			Items.Add(key, value);
+				Items.Add(key, value);
+			}
+		}
+
+		public static void Serialize()
+		{
+			lock (modSyncObject)
+			{
+
+			}
 		}
 
 		public static object Get(object key)
 		{
-			if (Items.ContainsKey(key))
-				return Items[key];
-
+			lock (modSyncObject)
+			{
+				if (Items.ContainsKey(key))
+					return Items[key];
+			}
 			return null;
+		}
+
+		static void Test()
+		{
 		}
 	}
 }
